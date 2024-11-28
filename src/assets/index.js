ebf65d755440760758baa9b01ff1182155ac7faa
aMaldonadoJ <amaldonado@simomexico.com>
2024-11-27 20:14:49 -0600

    Updates

--- src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import LogoIbero from "../assets/LogoIbero.svg"
-import FondoIbero from "../assets/FondoIbero.png"
-import arrowRight from "../assets/arrowRight.svg"
-
-export{
-    LogoIbero,
-    FondoIbero,
-    arrowRight,
-}
